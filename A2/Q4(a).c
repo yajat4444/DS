2f1820561e1785e5aab7ca4485a4797110135a35
yajat4444 <61344107+yajat4444@users.noreply.github.com>
2025-08-19 10:24:08 +0530

    Create Q4(a).c

---
+++ A2/Q4(a).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[100], s2[100];
+    scanf("%s", s1);
+    scanf("%s", s2);
+    strcat(s1, s2);
+    printf("%s", s1);
+    return 0;
+}
