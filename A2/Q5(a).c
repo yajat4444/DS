434c524586306d4eca65ee75a45e6a96d790d4a7
yajat4444 <61344107+yajat4444@users.noreply.github.com>
2025-09-01 20:39:53 +0530

    Update Q5(a).c

--- A2/Q5(a).c
+++ A2/Q5(a).c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
+    //normal diagonal matrix input
     int n;
     scanf("%d",&n);
     int matrix[n][n];
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
-        scanf("%d",&matrix[i][j];
+        scanf("%d",&matrix[i][j]);
       }
     }
+    //printing the normal matrix
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        printf("%d ",matrix[i][j]);
+      }
+      printf("\n");
+    }
+
     
     int diag[n];
+    printf("\n");
 
     for (int i = 0; i < n; i++) {
         diag[i] = matrix[i][i];
+    }
+
+    //printing the diagonal elements    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", diag[i]);
     }
 
     return 0;
