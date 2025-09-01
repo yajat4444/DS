b9d1a9e4c9d8da17f805aa5137fa75342dffb544
yajat4444 <61344107+yajat4444@users.noreply.github.com>
2025-09-01 21:03:10 +0530

    Create Q5(e).c

---
+++ A2/Q5(e).c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of matrix ");
+    scanf("%d",&n);
+
+    int sym[n*(n+1)/2];
+    int k=0;
+
+    for(int d=0; d<n; d++) {
+        if(d==0) {
+            printf("Enter %d elements of main diagonal\n", n);
+        } else {
+            printf("Enter %d elements of %d diagonal below main\n", n-d, d);
+        }
+        for(int i=0; i<n-d; i++) scanf("%d",&sym[k++]);
+    }
+
+    int matrix[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) matrix[i][j]=0;
+    }
+
+    k=0;
+    for(int d=0; d<n; d++) {
+        for(int i=0; i<n-d; i++) {
+            int row=i+d, col=i;
+            matrix[row][col] = sym[k];
+            matrix[col][row] = sym[k]; // symmetric property
+            k++;
+        }
+    }
+
+    printf("\nNormal Matrix:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) printf("%d ",matrix[i][j]);
+        printf("\n");
+    }
+
+    printf("\nSymmetric\n");
+    for(int i=0;i<n*(n+1)/2;i++) printf("%d ",sym[i]);
+
+    return 0;
+}
