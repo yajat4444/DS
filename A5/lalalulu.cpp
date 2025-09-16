061f07336b20cdfd294db20d3e1da88d2716f969
yajat4444 <61344107+yajat4444@users.noreply.github.com>
2025-09-16 11:50:17 +0530

    Delete A5

--- A5/lalalulu.cpp
+++
@@ -1,8 +0,0 @@
-A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-1 2 3 4 5 6 7 8 9 0
-bhai log out krdiya kr
-mai abhi teri dash dash kar skta tha sabkuch delete krke
-par kar nhi rha dil ka saaf hu
-yaad rkhio bhai
-agar meri jagah koi aur hota to pta nhi kya kya krskta tha
-tera gmail bhi khula hua hai ispr
